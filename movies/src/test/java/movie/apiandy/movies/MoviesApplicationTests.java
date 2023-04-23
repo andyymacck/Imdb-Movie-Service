067e1399ee009e69983ab7d37cb5cbe6cfb683c4
Andrew Mackay <89171542+andyymacck@users.noreply.github.com>
2023-04-22 23:22:41 -0300

    Add files via upload

---
+++ movies/src/test/java/movie/apiandy/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.apiandy.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
